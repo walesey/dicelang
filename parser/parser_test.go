ec66c16f3066e736a154f57a42e982afdff52c2e
walesey <joshua.wales@gmail.com>
2018-04-16 18:22:00 +1000

    Fixed tests

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"bytes"
+	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,7 +13,9 @@ func Test_Parser_Execute(t *testing.T) {
 	expected := `[{"V": 0, "P": 0.0625}, {"V": 1, "P": 0.25}, {"V": 2, "P": 0.375}, {"V": 3, "P": 0.25}, {"V": 4, "P": 0.0625}]`
 	result, err := parser.Execute()
 	assert.Nil(t, err)
-	assert.JSONEq(t, expected, result)
+	jsonResult, err := json.Marshal(result)
+	assert.Nil(t, err)
+	assert.JSONEq(t, expected, string(jsonResult))
 }
 
 func Test_Parser_Execute_Aggregate(t *testing.T) {
@@ -20,5 +23,7 @@ func Test_Parser_Execute_Aggregate(t *testing.T) {
 	expected := `[{"V":1,"P":0.0417},{"V":2,"P":0.125},{"V":3,"P":0.1667},{"V":4,"P":0.1667},{"V":5,"P":0.1667},{"V":6,"P":0.1667},{"V":7,"P":0.125},{"V":8,"P":0.0417}]`
 	result, err := parser.Execute()
 	assert.Nil(t, err)
-	assert.JSONEq(t, expected, result)
+	jsonResult, err := json.Marshal(result)
+	assert.Nil(t, err)
+	assert.JSONEq(t, expected, string(jsonResult))
 }
